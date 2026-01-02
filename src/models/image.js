14c81afca7732970264ce7ef8a15019b99af981d
robloxzone001 <robloxzone486@gmail.com>
2026-01-02 21:34:29 +0530

    Add files via upload

---
+++ src/models/image.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const ImageSchema = new mongoose.Schema({
+  url: { type: String, required: true },
+  public_id: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Image", ImageSchema);
